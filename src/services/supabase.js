c88e4ff41600eab3845793c92abb6a3bee3c60b5
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-10-26 23:03:43 +0530

    initilize supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://mywnvryfrakhcxxolupn.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15d252cnlmcmFraGN4eG9sdXBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk5NTI1NjAsImV4cCI6MjA0NTUyODU2MH0.u5hM7hJWBsd_Qa9Kd5rxSo4F8KXXpiFC_MJq594FgpM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
