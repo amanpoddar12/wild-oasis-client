1d1dfc94671d8b7f3d6a174bb92775a966183a0b
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-11-01 23:58:07 +0530

    commit

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -3,10 +3,17 @@ import Select from "./Select";
 
 export default function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const sortBy = searchParams.get("sortBy") || "";
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
     setSearchParams(searchParams);
   }
-  return <Select options={options} type="white" onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type="white"
+      onChange={handleChange}
+    />
+  );
 }
