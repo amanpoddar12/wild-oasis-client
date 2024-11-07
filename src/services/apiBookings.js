9c65f5e503f57c6b818dbd75bc79fa804d890d20
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-11-07 23:29:26 +0530

    commit

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,7 +7,8 @@ export async function getBookings() {
     .from("bookings")
     .select(
       "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, guests(fullName,email)"
-    );
+    )
+    .eq("status", "unconfirmed");
   // let cabins = getCabins();
   // console.log(cabins);
   // let query = supabase
