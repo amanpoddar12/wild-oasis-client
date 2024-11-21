3adfe370faa5770c9833d69ef50824dd5aa194da
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-11-22 00:21:41 +0530

    create new user

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,15 @@
 import supabase from "./supabase.js";
 
+export const signup = async ({ email, password, fullName }) => {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: { data: { fullName, avatar: "" } },
+  });
+  if (error) throw new Error(error.message);
+  return data;
+};
+
 export const login = async ({ email, password }) => {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
