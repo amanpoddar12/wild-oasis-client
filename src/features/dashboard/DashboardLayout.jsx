3b4b126737359cc8769327f9a4d0525a050d206d
amanpoddar-dev12 <amanpoddar775@gmail.com>
2024-11-23 13:28:17 +0530

    working on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,23 @@
 import styled from "styled-components";
-
+import useRecentBookings from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+export default function DashboardLayout() {
+  const { isLoading, bookings } = useRecentBookings();
+  if (isLoading) return <Spinner />;
+  console.log(bookings);
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
